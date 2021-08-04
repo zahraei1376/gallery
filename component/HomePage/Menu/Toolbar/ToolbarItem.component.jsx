f7018902f6fe58747ab23909d7129e6cddf61ab9
zahraei1376 <alipour1418@gmail.com>
2021-08-04 21:28:39 +0430

    add navigation

--- component/HomePage/Menu/Toolbar/ToolbarItem.component.jsx
+++ component/HomePage/Menu/Toolbar/ToolbarItem.component.jsx
@@ -2,57 +2,76 @@ import {ToolbarContainer,ToolbarInfoContainer,ToolbarInfo,DrawerContainer,Toolba
     IconContainer,MyListIcon ,MyCloseIcon,ListContainer,ToolbarInfoText,List ,
     ListItem ,ListImg,ImgContainer,MyNavLink,ArrowIcon,ArrowIconButton,ListLink,
     ConnectionBox,ConnectionIcon,TitleBox,SubTitle,Title} from './toolbar.styles';
+
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
 const ToolbarItems = ({clicked}) =>{
     return(
         <ToolbarInfoBack show = {clicked ? true : null}>
         <ToolbarInfoContainer show = {clicked ? true : null}>
-          <ToolbarInfoText show = {clicked ? true : null}>
-            <ConnectionBox>
-              <ConnectionIcon/>
-              <ConnectionIcon/>
-              <ConnectionIcon/>
-              <ConnectionIcon/>
-            </ConnectionBox>
-            <TitleBox>
-                <Title>سفارش غذای آنلاین</Title>
-                <SubTitle>آسان،راحت ، بدون زحمت</SubTitle>
-            </TitleBox>
-          </ToolbarInfoText>
-
           <ToolbarInfo show = {clicked ? true : null}>
             <ListContainer>
               <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
-                <IconContainer><MyListIcon/></IconContainer> 
                 <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
+                <IconContainer><MyListIcon/></IconContainer> 
               </ListItem>
+
               <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
-                <IconContainer><MyListIcon/></IconContainer> 
                 <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
-              </ListItem>
-            </ListContainer>
-            <ListContainer>
-              <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
                 <IconContainer><MyListIcon/></IconContainer> 
-                <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
-              </ListItem>
-              <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
-                <IconContainer><MyListIcon/></IconContainer> 
-                <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
               </ListItem>
             </ListContainer>
+
             <ListContainer>
               <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
-                <IconContainer><MyListIcon/></IconContainer> 
                 <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
+                <IconContainer><MyListIcon/></IconContainer> 
               </ListItem>
               <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
+              <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
                 <IconContainer><MyListIcon/></IconContainer> 
+              </ListItem>
+            </ListContainer>
+
+            <ListContainer>
+              <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
                 <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
+                <IconContainer><MyListIcon/></IconContainer> 
+              </ListItem>
+              <ListItem onClick={()=>handleUrl('/managerTextWeblog')}>
+                <MyNavLink href ='/managerTextWeblog'>مدیریت منابع</MyNavLink>
+                <IconContainer><MyListIcon/></IconContainer> 
               </ListItem>
             </ListContainer>
           </ToolbarInfo>
           
+          <ToolbarInfoText show = {clicked ? true : null}>
+            <ConnectionBox>
+              <InstagramIcon style={{fontSize:'2.5rem',
+                color:'#fff', }}/>
+
+              <TelegramIcon style={{fontSize:'2.5rem',
+                color:'#fff',marginLeft:'1.5rem' }} />
+
+              <FacebookIcon style={{fontSize:'2.5rem',
+                color:'#fff', marginLeft:'1.5rem'}} />
+
+              <MailOutlineIcon style={{fontSize:'2.5rem',
+                color:'#fff',marginLeft:'1.5rem' }} />
+              {/* <ConnectionIcon/>
+              <ConnectionIcon/>
+              <ConnectionIcon/> */}
+            </ConnectionBox>
+            <TitleBox>
+                <Title>طبیعت ، دنیای ما</Title>
+                <SubTitle>دنیای حیوانات</SubTitle>
+            </TitleBox>
+          </ToolbarInfoText>
         </ToolbarInfoContainer>
+        
         </ToolbarInfoBack>
     )
 }
--- component/HomePage/Menu/Toolbar/toolbar.styles.jsx
+++ component/HomePage/Menu/Toolbar/toolbar.styles.jsx
@@ -1,110 +1,4 @@
-// .Top{
-//     width: 100%;
-//     height: 60px;
-//     background-color: #1b1e21;
-// }
-// .toolbar{
-//     line-height: 0;
-//     background-color: #322F32;
-//     /* background-color:
-//     #181818; */
-//     border-width: 0 0 0.14285714285714285rem 0;
-//     border-bottom: 4px solid #521751;
-//     border-style: solid;
-//     /*  */
-//     height: 56px;
-//     width: 100%;
-//     top: 0;
-//     left: 0;
-//     box-shadow:0px 0px 15px 0px #000000;
-//     /* background-color: #521751; */
-//     /*display: flex;*/
-//     /*justify-content: space-between;*/
-//     /*align-items: center;*/
-//     /*padding: 0px 20px;*/
-//     /*box-sizing: border-box;*/
-//     /*z-index: 100;*/
-// }
-// .toolbar__navigation{
-//     display: flex;
-//     height: 100%;
-//     align-items: center;
-//     padding: 0 1rem;
-//     font-size: 1rem;
-// }
-// .toolbar__logo{
-//     margin-left: 1rem;
-// }
-// .toolbar__logo a{
-//     float: left;
-//     color: white;
-//     text-decoration: none;
-//     font-size: 1.5rem;
-// }
-// .toolbar_navigation_items ul{
-//     list-style: none;
-//     margin: 0;
-//     padding: 0;
-//     display: flex;
-// }
-// .toolbar_navigation_items li{
-//     padding: 0 1.5rem;
-// }
-// .toolbar_navigation_items a{
-//     color: white;
-//     text-decoration: none;
-// }
-// .spacer{
-//    flex: 1;
-// }
-// .toolbar_navigation_items a:hover,
-// .toolbar_navigation_items a:active,
-// .toolbar_navigation_items a.active{
-//     color: #DE94EA;
-//     /* color: #fa923f; */
-// }
 
-// .sizze{
-//     color: white;
-//     margin-left: 10px;
-// }
-
-// @media (max-width: 768px) {
-//     .toolbar_navigation_items{
-//         display: none;
-//     }
-//     /*.toolbar__logo{*/
-//     /*    margin-left:10px;*/
-//     /*    margin-top: 30px;*/
-//     /*}*/
-    
-// }
-// @media (max-width: 900px) {
-//     .toolbar__navigation{
-//         display: flex;
-//         height: 100%;
-//         align-items: center;
-//         padding: 0 .5rem;
-//     }
-
-// }
-// @media (min-width: 768px) {
-//     .toolbar__toggle-button{
-//         display: none;
-//     }
-//     .toolbar__logo{
-//         margin-left:0px;
-//     }
-
-// }
-
-
-
-// /*.Toolbar nav{*/
-// /*    height: 100%;*/
-// /*}*/
-
-////////////////////////////////
 import styled ,{css} from "styled-components";
 import  {NavLink} from 'react-router-dom';
 import ListAltIcon from '@material-ui/icons/ListAlt';
@@ -113,8 +7,9 @@ import CloseIcon from '@material-ui/icons/Close';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import { IconButton } from '@material-ui/core';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
-///////////////////////////////
+
 export const Title = styled.h1`
+    width:100%;
     color:#fff;
     font-size:4rem;
     text-align:right;
@@ -124,21 +19,20 @@ export const SubTitle = styled.h4`
     color:#fff;
     font-size:2rem;
     text-align:right;
+    width:100%;
 `;
 
 export const ToolbarContainer = styled.div`
-    // width:100%;
 `;
 
 export const ToolbarInfoBack = styled.div`
     width:100%;
     background-color: #1d1d1d;
     transition:all .3s;
-    // display: none;
     height:${props => props.show ? '40rem' : '0'};
-    // transform: ${props => props.show ? 'scale(7)' : 'scale(0)'};
     position:fixed;
     top:0;
+    left:0;
     z-index:1;
 `;
 export const ToolbarInfoContainer = styled.div`
@@ -150,59 +44,44 @@ export const ToolbarInfoContainer = styled.div`
     transform: translate(-50%,-50%);
     list-style: none;
     text-align: center;
-    // width: 100%;
     width:${props => props.show ? '100%' : '0'};
-    // transform: ${props => props.show ? 'scale(1)' : 'scale(0)'};
-    // transform: ${props => props.show ? 'scale(7)' : 'scale(0)'};
-    // background-color: #fff;
     z-index: 1000;
     display:flex;
     justify-content:center;
     align-items:center;
     color:#fff;
-    
-    // background-color: #1d1d1d;
-    // transition:all .3s;
-    // // display: none;
-    // height:${props => props.show ? '10rem' : '0'};
-    // transform: ${props => props.show ? 'scale(7)' : 'scale(0)'};
-    // position:absolute;
-    // top:0;
-    // z-index:1;
 `;
 
 export const ToolbarInfo = styled.div`
-    // list-style:none;
-    // width:0;
     display: ${props => props.show ? 'block' : 'none'};
     opacity:${props => props.show ? '1' : '0'};
     width:${props => props.show ? '50%' : '0'};
-    // display:flex;
-    // justify-content:center;
-    // align-items:center;
 `;
 
 export const ToolbarInfoText = styled.div`
     width:${props => props.show ? '50%' : '0'};
     display:${props => props.show ? 'flex' : 'none'};
-    // width:50%;
-    // display:flex;
+    margin-top:3rem;
     flex-direction:column;
     justify-content:center;
-    align-items:center;
+    text-align:right;
+    padding:0 10rem 0 3rem;
 `;
 
 export const ListContainer = styled.div`
-    // list-style:none;
     display:grid;
     grid-template-columns: auto auto;
     grid-column-gap: 50px;
+    
     z-index:4;
+    @media only screen and (max-width: 645px){
+        grid-column-gap: 14px;
+        grid-row-gap: 14px;
+    }
 `;
 
 export const ImgContainer = styled.div`
     width:100%;
-    // height:10rem;
     margin:1rem 0;
     display:flex;
     justify-content:center;
@@ -222,8 +101,6 @@ export const DrawerContainer = styled.div`
 `;
 
 const IconsCss = css`
-    // font-size:2rem;
-    // color:#bf4f7b;
     color:rgb(218,160,103);
 `;
 
@@ -236,31 +113,18 @@ export const ArrowIcon = styled(MenuIcon)`
     ${IconsCss};
     color:rgb(218,160,103);
     font-size:2.5rem !important;
-
-    // @media only screen and (max-width: 604px){
-    //     font-size:2rem !important;
-    // }
-
 `;
 
 export const MyCloseIcon = styled(CloseIcon)`
     ${IconsCss};
     color:rgb(218,160,103);
     font-size:2.5rem !important;
-
-    // @media only screen and (max-width: 604px){
-    //     font-size:2rem !important;
-    // }
-
 `;
 
 
 
 export const ArrowIconButton = styled(IconButton)`
     color:rgb(218,160,103);
-    // font-size:2.5rem;
-    // width:3rem !important;
-    // height:3rem !important;
     border-radius:5px !important;
     margin:1rem 2rem !important;
     border:1px solid rgb(218,160,103) !important;
@@ -268,11 +132,6 @@ export const ArrowIconButton = styled(IconButton)`
     position:relative;
     z-index:3;
     transition:all .3s;
-
-    // @media only screen and (max-width: 604px){
-    //     width:2rem !important;
-    //     height:2rem !important;
-    // }
 
     &:active,
     &:focus{
@@ -282,7 +141,6 @@ export const ArrowIconButton = styled(IconButton)`
     &:hover{
         border:1px solid rgb(218,160,103) !important;
         background-color:rgb(218,160,103) !important;
-        // transform:scale(1.05);
     }
 
     &:hover ${ArrowIcon}{
@@ -293,19 +151,6 @@ export const ArrowIconButton = styled(IconButton)`
         color:#fff !important;
     }
 `;
-
-
-
-// export const MyNavLink = styled(NavLink)`
-//     text-decoration: none;
-//     color:#000;
-//     font-family:Bnazanin;
-//     font-size:2rem;
-
-//     &:hover{
-//         text-decoration: none;
-//     }
-// `;
 
 export const MyNavLink = styled.a`
     text-decoration: none;
@@ -331,16 +176,35 @@ export const List = styled.ul`
 `;
 
 export const ListItem = styled.div`
-    padding: 0 1.5rem;
+    width:100%;
+    padding: 1.5rem 2rem;
     display:flex;
     align-items:center;
-    // padding:1rem;
+    justify-content:center;
     transition:all .3s;
     color:#fff;
-    &:hover{
-        // background-color:#bf4f7b;
-        background-color:rgb(218,160,103);
-        color:#fff;
+    position:relative;
+    z-index: 1;
+
+    &::before{
+        content: "";
+        position: absolute;
+        display: block;
+        top: 0;
+        right: 0;
+        width:0;
+        height: 0;
+        background-color:#bf4f7b;
+        z-index: -1;
+        // transition:all .3s;
+        transition: transform 0.2s, width 0.4s cubic-bezier(1, 0, 0, 1) 0.2s, background-color 0.1s;
+    }
+
+    &:hover::before{
+        width:100%;
+        height: 100%;
+        // background-color:rgb(218,160,103);
+        // color:#fff;
     }
 
     &:hover a{
@@ -355,6 +219,7 @@ export const ListItem = styled.div`
     &:hover ${ArrowIcon}{
         color:#fff;
     }
+
 `;
 
 export const ListLink = styled.a`
@@ -362,7 +227,7 @@ export const ListLink = styled.a`
     text-decoration: none;
 `;
 
-export const ConnectionIcon = styled(AssignmentIndIcon)`
+export const ConnectionIcon = styled(AssignmentIndIcon) `
     font-size:2.5rem !important;
     color:#fff;
 
@@ -372,9 +237,9 @@ export const ConnectionIcon = styled(AssignmentIndIcon)`
 `;
 
 export const ConnectionBox = styled.div`
-    display:flex;
-    justify-content:center;
-    align-items:center;
+    border-bottom:1px solid rgb(218,160,103);
+    padding-bottom:2rem;
+    margin-bottom:1rem;
 `;
 
 export const TitleBox = styled.div`
